6a39ff3023fff51fa0051211dc1e00547523ed48
ozmberkan <ozmberkan@gmail.com>
2024-12-03 18:07:41 +0300

    premium box's colors changed

--- frontend/src/components/Rightbar/Rightbar.jsx
+++ frontend/src/components/Rightbar/Rightbar.jsx
@@ -4,7 +4,7 @@ import Pro from "../UI/Rightbar/Pro";
 
 const Rightbar = () => {
   return (
-    <div className="min-w-80 max-w-64 p-5 flex flex-col items-start justify-start border-r gap-4 border-l">
+    <div className="min-w-80 max-w-64 p-5 flex flex-col items-start justify-start border-r gap-4 border-l draggable-container">
       <form>
         <div className="flex items-center gap-x-2 border rounded-xl px-4 h-10">
           <span>
--- frontend/src/components/UI/Rightbar/Pro.jsx
+++ frontend/src/components/UI/Rightbar/Pro.jsx
@@ -6,22 +6,13 @@ const Pro = () => {
   return (
     <div className="p-3 rounded-xl h-[150px] w-full bg-premium bg-opacity-1  bg-center bg-cover flex flex-col gap-3 shadow-xl">
       <div className="flex flex-col">
-        <h1 className="font-extrabold text-neutral-700 text-2xl">
-          Pro'ya geç!
-        </h1>
-        <p className="text-xs font-bold mt-2 text-neutral-700">
-          Hemen{" "}
-          <strong className="bg-clip-text text-transparent bg-gradient-to-r from-[#28543C] to-[#AF261C] relative ">
-            yılbaşı
-            <span className="text-neutral-700 absolute -top-0.5 -right-1">
-              <IoIosSnow />
-            </span>
-          </strong>{" "}
-          indiriminden yararlanın ve pro'ya geçin!
+        <h1 className="font-extrabold text-white text-2xl">Pro'ya geç!</h1>
+        <p className="text-xs font-bold mt-2 text-neutral-100">
+          Hemen yılbaşı indiriminden yararlanın ve pro'ya geçin!
         </p>
       </div>
       <div className="mt-2">
-        <Link className="bg-primary text-white border text-sm font-semibold px-4 py-2 rounded-xl ">
+        <Link className="bg-primary text-white  text-sm font-semibold px-4 py-2 rounded-xl ">
           Detay
         </Link>
       </div>
