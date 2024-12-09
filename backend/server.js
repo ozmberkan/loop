7ec5a66f463ee3f1a109aa5057be073ee3f78baa
ozmberkan <ozmberkan@gmail.com>
2024-12-09 18:01:47 +0300

    cors added

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://loop-tan.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "https://loop-tan.vercel.app",
+      "https://www.loop-tan.vercel.app",
+    ],
     credentials: true,
   })
 );
