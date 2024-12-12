2d4498c0a39c5d28df678f531f2acca27bbece72
ozmberkan <ozmberkan@gmail.com>
2024-12-12 16:21:59 +0300

    fixed home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,15 +4,20 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Post from "~/components/Post/Post";
 import PostAddModal from "~/components/UI/Modals/PostAddModal";
+import { useAccount } from "~/hooks/useAccount";
 import { usePosts } from "~/hooks/usePosts";
 import { getAllPost } from "~/redux/slices/postsSlice";
+import { getUserById } from "~/redux/slices/usersSlice";
 
 const Home = () => {
   const [postModal, setPostModal] = useState(false);
   const dispatch = useDispatch();
+
+  const user = useAccount();
   ring.register();
   useEffect(() => {
     dispatch(getAllPost());
+    dispatch(getUserById(user._id));
   }, []);
 
   const posts = usePosts();
@@ -42,9 +47,13 @@ const Home = () => {
           </button>
         </div>
         <div className="grid grid-cols-2 gap-10">
-          {posts.map((post) => (
-            <Post post={post} />
-          ))}
+          {posts.length > 0 ? (
+            posts.map((post) => <Post post={post} />)
+          ) : (
+            <div className="grid-cols-2 bg-primary/20 text-primary border-primary px-4 py-2 rounded-lg border">
+              Herhangi bir gönderi bulunamadı!
+            </div>
+          )}
         </div>
       </div>
     </>
